03baa4aef35ad89bf5c2faa60092652fc8c1a7a1
Samuel <samuelreyesiglesias@gmail.com>
2022-04-29 17:01:41 -0600

    Agregar archivos de proyecto.

---
+++ TheContacts/TheContacts/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheContacts.Models
+{
+    public class Contact
+    {
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Foto { get; set; }
+    }
+}
---
+++ TheContacts/TheContacts/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using TheContacts.Models;
+
+namespace TheContacts.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<Contact> Contacts { get; set; }
+        =new ObservableCollection<Contact>();
+
+        public MainPageViewModel()
+        {
+            //Contacts = new Faker<Contact>();
+
+        }
+    }
+}
